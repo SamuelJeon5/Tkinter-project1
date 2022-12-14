0e91710bd9c289a1ef941ac9116c9356424d4be0
Samuel Jeon <soulstar67@gmail.com>
2022-12-14 13:02:23 -0600

    Update frame

--- gui_basic/1_create_frame.py
+++ gui_basic/1_create_frame.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 root = Tk()
 root.title("Samuel GUI")
-
+root.geometry("640x480") #가로 x 세로
+# root.geometry("640x480+100+300") # 가로 세로 x좌표 y좌표
+root.resizable(False, False) # 너비, 높이 값 변경 블가
 
 root.mainloop()
- 
